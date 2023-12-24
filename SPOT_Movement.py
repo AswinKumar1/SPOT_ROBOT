ca9fd31a0ecf788ea34c6831c52bc380113bcb5c
Aswin Kumar <achu4798@gmail.com>
2023-12-23 21:39:02 -0500

    Create SPOT_Movement.py

---
+++ SPOT_Movement.py
@@ -0,0 +1,7 @@
+#!/env/bin/env python3
+
+import rclpy
+import json
+from rclpy.node import Node
+from spot_msgs.msg import BatteryStateArray
+from spot_msgs.msg import String  # Import String message type for the new topic
