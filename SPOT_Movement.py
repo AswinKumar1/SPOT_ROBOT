8a27ecd2c0a1ee6a493d75d27ef633a3183e15ce
Aswin Kumar <achu4798@gmail.com>
2024-01-01 19:25:25 -0500

    Update SPOT_Movement.py

--- SPOT_Movement.py
+++ SPOT_Movement.py
@@ -1,7 +1,30 @@
 #!/env/bin/env python3
 
 import rclpy
-import json
 from rclpy.node import Node
-from spot_msgs.msg import BatteryStateArray
-from spot_msgs.msg import String  # Import String message type for the new topic
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+class SpotMovementController(Node):
+  def __init__(self):
+    self.command_pub_ = self.create_publisher(Twist, "/cmd_vel", 10)
+
+  def send_velocity_command(self, linear_x, angular_z):
+    twist_msg = Twist()
+    twist_msg.linear_x = linear_x
+    twist_msg.angular_z = angular_z
+    self.publisher_.publish(twist_msg)
+    self.get_logger().info("Sent Velocity Command: linear={linear_x), angular={angular_z}")
+
+def main(args=None)
+  rclpy.init(args=args)
+  node = SpotMovementController()
+  try:
+    node.send_velocity_command(target_x=1.0, target_y=0.0)
+    rclpy.spin(node)
+  except KeyboardInterrupt:
+    pass
+
+if __name__ == '__main__'
+  main()
+
