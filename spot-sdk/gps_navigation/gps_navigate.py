fb6e481eebaed79a114d56c636a58697cd2f0103
Aswin Kumar <achu4798@gmail.com>
2024-06-26 22:18:30 -0400

    Update gps_navigate.py

--- spot-sdk/gps_navigation/gps_navigate.py
+++ spot-sdk/gps_navigation/gps_navigate.py
@@ -1,151 +1,171 @@
-import logging
 import math
 import sys
 import time
-import pymongo
-from geopy.distance import geodesic
-
 import bosdyn.client
 import bosdyn.client.util
-from bosdyn.api import basic_command_pb2
-from bosdyn.api import geometry_pb2 as geo
-from bosdyn.api.basic_command_pb2 import RobotCommandFeedbackStatus
-from bosdyn.client import math_helpers
-from bosdyn.client.frame_helpers import (BODY_FRAME_NAME, ODOM_FRAME_NAME, VISION_FRAME_NAME,
-                                         get_se2_a_tform_b)
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient, blocking_stand, blocking_sit
 from bosdyn.client.lease import LeaseClient, LeaseKeepAlive
-from bosdyn.client.robot_command import (RobotCommandBuilder, RobotCommandClient,
-                                         block_for_trajectory_cmd, blocking_stand)
-from bosdyn.client.robot_state import RobotStateClient
+from bosdyn.api.spot import robot_command_pb2 as spot_command_pb2
+from bosdyn.api import geometry_pb2
 
-_LOGGER = logging.getLogger(__name__)
+# Function to calculate the Haversine distance
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371.0  # Earth radius in kilometers
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
+    a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c * 1000   # Convert to meters
+    return distance
 
-# MongoDB connection details
-db_hostname = '192.168.x.xx'
-db_port = 27017
+# Function to calculate the bearing
+def calculate_bearing(lat1, lon1, lat2, lon2):
+    dlon = math.radians(lon2 - lon1)
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+    x = math.sin(dlon) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dlon)
+    bearing = math.atan2(x, y)
+    bearing = math.degrees(bearing)
+    bearing = (bearing + 360) % 360
+    return bearing
+
+# Function to convert track angle to cardinal direction
+def track_to_direction(track):
+    directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 
+                  'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
+    idx = round(track / 22.5) % 16
+    return directions[idx]
+
+class SpotWrapper:
+    def __init__(self, spot_init=True):
+        self.sdk = None
+        self.robot = None
+        if spot_init:
+            # Initialize Spot Robot if required
+            self.sdk = bosdyn.client.create_standard_sdk('MoveSpot')
+            self.robot = self.sdk.create_robot('192.168.1.10')
+            self.robot.authenticate('admin', 'Cards@2023spot')
+            self.robot.time_sync.wait_for_sync()
+            lease_client = self.robot.ensure_client(LeaseClient.default_service_name)
+            lease = lease_client.take()
+            lease_keep_alive = LeaseKeepAlive(lease_client)
+            self.robot.power_on(timeout_sec=20)
+            assert self.robot.is_powered_on(), "Robot failed to power on"
+
+    def stand_spot(self):
+        try:
+            command_client = self.robot.ensure_client(RobotCommandClient.default_service_name)
+            blocking_stand(command_client, timeout_sec=15)
+        except Exception as e:
+            print(f"Error standing Spot robot: {e}")
+
+    def sit_spot(self):
+        try:
+            command_client = self.robot.ensure_client(RobotCommandClient.default_service_name)
+            blocking_sit(command_client, timeout_sec=10)
+        except Exception as e:
+            print(f"Error sitting Spot robot: {e}")
+
+    def move_spot(self, goal_x, goal_y, duration_s):
+        try:
+            max_linear_velocity = geometry_pb2.Vec2(x=0.5, y=0.0)
+            max_angular_velocity = 0.0
+
+            max_se2_velocity = geometry_pb2.SE2Velocity(linear=max_linear_velocity, angular=max_angular_velocity)
+
+            velocity_limit = geometry_pb2.SE2VelocityLimit(
+                max_vel=max_se2_velocity
+            )
+
+            mobility_params = spot_command_pb2.MobilityParams(
+                locomotion_hint=spot_command_pb2.HINT_AUTO,
+                vel_limit=velocity_limit
+            )
+
+            frame_tree_snapshot = self.robot.get_frame_tree_snapshot()
+            command_client = self.robot.ensure_client(RobotCommandClient.default_service_name)
+            command = RobotCommandBuilder.synchro_trajectory_command_in_body_frame(goal_x_rt_body=goal_x, goal_y_rt_body=goal_y, goal_heading_rt_body=0.0, frame_tree_snapshot=frame_tree_snapshot, params=mobility_params, body_height=0.0, build_on_command=None)
+            command_client.robot_command(command, end_time_secs=time.time() + duration_s)
+
+        except Exception as e:
+            print(f"Error moving Spot robot: {e}")
+
+    def turn_spot(self, target_angle_degrees, duration_s):
+        try:
+            command_client = self.robot.ensure_client(RobotCommandClient.default_service_name)
+            target_angle_radians = math.radians(target_angle_degrees)
+
+            # Calculate angular velocity
+            max_angular_velocity = 1.6  # radians per second
+            angular_velocity = min(target_angle_radians / duration_s, max_angular_velocity)
+
+            # Adjust duration if necessary
+            actual_duration = target_angle_radians / angular_velocity
+
+            command = RobotCommandBuilder.synchro_velocity_command(v_x=0.0, v_y=0.0, v_rot=angular_velocity)
+            end_time = time.time() + actual_duration
+            print(f"Sending turn command with v_rot={angular_velocity} radians/sec for duration_s={actual_duration} seconds (end_time={end_time})")
+
+            # Use synchronous command for testing
+            command_client.robot_command(command, end_time_secs=end_time)
+
+            print("Turn command sent successfully")
+        except Exception as e:
+            print(f"Error turning Spot robot: {e}")
+
+    def stop_spot(self):
+        try:
+            command_client = self.robot.ensure_client(RobotCommandClient.default_service_name)
+            command = RobotCommandBuilder.synchro_velocity_command(v_x=0.0, v_y=0.0, v_rot=0.0)
+            command_client.robot_command(command)
+        except Exception as e:
+            print(f"Error stopping Spot robot: {e}")
 
 def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    bosdyn.client.util.add_base_arguments(parser)
-    parser.add_argument('hostname', help='Hostname or IP address of the robot.')
-    parser.add_argument('--frame', choices=[VISION_FRAME_NAME, ODOM_FRAME_NAME],
-                        default=ODOM_FRAME_NAME, help='Send the command in this frame.')
-    parser.add_argument('--stairs', action='store_true', help='Move the robot in stairs mode.')
-    options = parser.parse_args()
-    bosdyn.client.util.setup_logging(options.verbose)
-
-    # Create robot object.
-    sdk = bosdyn.client.create_standard_sdk('SpotGPSClient')
-    robot = sdk.create_robot(options.hostname)
-    
     try:
-        robot.authenticate('xxxxx', 'xxxxxxxxx')
-    except bosdyn.client.exceptions.ProxyConnectionError as e:
-        print(f"Failed to authenticate: {e}")
-        sys.exit(1)
+        spot = SpotWrapper()
+        spot.stand_spot()
 
-    # Check that an estop is connected with the robot so that the robot commands can be executed.
-    assert not robot.is_estopped(), 'Robot is estopped. Please use an external E-Stop client, ' \
-                                    'such as the estop SDK example, to configure E-Stop.'
+        print("The command executed here")
 
-    # Create the lease client.
-    lease_client = robot.ensure_client(LeaseClient.default_service_name)
+        # Fetching GPS coordinates and calculating turn angle and distance
+        DEST_LAT = 39.260150
+        DEST_LON = -76.713600
+        curr_lat = 39.260130
+        curr_lon = -76.714656
 
-    # Setup clients for the robot state and robot command services.
-    robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
-    robot_command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        track = calculate_bearing(curr_lat, curr_lon, DEST_LAT, DEST_LON)
+        print("Track: %s" % track)
+        turn_angle = (track - 90) % 360  # Assuming a turn 90 degrees left from the current direction
 
-    with LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
-        # Power on the robot and stand it up.
-        robot.time_sync.wait_for_sync()
-        robot.power_on()
-        blocking_stand(robot_command_client)
+        # Adjust turn angle to one decimal place
+        rounded_turn_angle = round(turn_angle, 1)
 
-        # Get the robot's current GPS location and the target GPS location from MongoDB.
-        current_latitude, current_longitude, target_latitude, target_longitude = get_gps_locations_from_db()
-        print(f"Current GPS Location: {current_latitude}, {current_longitude}")
-        print(f"Target GPS Location: {target_latitude}, {target_longitude}")
-
-        # Convert GPS to the robot's frame.
-        dx, dy = gps_to_local_frame(current_latitude, current_longitude, target_latitude, target_longitude)
-
-        try:
-            return relative_move(dx, dy, 0, options.frame, robot_command_client, robot_state_client, stairs=options.stairs)
-        finally:
-            # Send a Stop at the end, regardless of what happened.
-            robot_command_client.robot_command(RobotCommandBuilder.stop_command())
-
-def get_gps_locations_from_db():
-    client = pymongo.MongoClient(db_hostname, db_port)
-    db = client["robot_messaging"]
-    collection = db["robots"]
-    try:
-        query = {'robot_name': "spot_2"}
-        robot_data = collection.find_one(query)
-        if robot_data:
-            current_latitude = robot_data.get("latitude")
-            current_longitude = robot_data.get("longitude")
-            target_latitude = robot_data.get("target_lat")
-            target_longitude = robot_data.get("target_lon")
+        if rounded_turn_angle > 90:
+            duration_s = int(rounded_turn_angle / 90)
         else:
-            raise ValueError("Robot GPS data not found in the database.")
-        
-        client.close()
-        return current_latitude, current_longitude, target_latitude, target_longitude
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+            duration_s = 1  # Default duration if not exceeded 90 degrees
 
-def gps_to_local_frame(current_latitude, current_longitude, target_latitude, target_longitude):
-    # Calculate the relative x, y distance in meters between current and target GPS coordinates.
-    current_location = (current_latitude, current_longitude)
-    target_location = (target_latitude, target_longitude)
-    distance = geodesic(current_location, target_location).meters
+        print(f"Turning robot by {rounded_turn_angle} degrees")
+        spot.turn_spot(rounded_turn_angle, duration_s)
 
-    # Assuming a simple flat-earth approximation for small distances
-    delta_lat = target_latitude - current_latitude
-    delta_lon = target_longitude - current_longitude
-    dx = delta_lat * 111139  # Convert latitude to meters
-    dy = delta_lon * (111139 * math.cos(math.radians(current_latitude)))  # Convert longitude to meters
+        print("The turn command executed here")
+        time.sleep(duration_s + 1)  # Ensure turn is completed before moving
 
-    return dx, dy
+        # Calculate distance to destination
+        distance_to_dest = haversine(curr_lat, curr_lon, DEST_LAT, DEST_LON)
+        print("Distance in meters: %s" % distance_to_dest)
+        goal_x = distance_to_dest  # Assuming moving towards the destination in x direction
+        goal_y = 0.0
+        duration = 20
+        print(f"Moving robot to position ({goal_x}, {goal_y}) for {duration} seconds")
+        spot.move_spot(goal_x, goal_y, duration)
+        return True
+    except Exception as exc:
+        print(f"An error occurred: {exc}")
+        return False
 
-def relative_move(dx, dy, dyaw, frame_name, robot_command_client, robot_state_client, stairs=False):
-    transforms = robot_state_client.get_robot_state().kinematic_state.transforms_snapshot
-
-    # Build the transform for where we want the robot to be relative to where the body currently is.
-    body_tform_goal = math_helpers.SE2Pose(x=dx, y=dy, angle=dyaw)
-    # We do not want to command this goal in body frame because the body will move, thus shifting
-    # our goal. Instead, we transform this offset to get the goal position in the output frame
-    # (which will be either odom or vision).
-    out_tform_body = get_se2_a_tform_b(transforms, frame_name, BODY_FRAME_NAME)
-    out_tform_goal = out_tform_body * body_tform_goal
-
-    # Command the robot to go to the goal point in the specified frame. The command will stop at the
-    # new position.
-    robot_cmd = RobotCommandBuilder.synchro_se2_trajectory_point_command(
-        goal_x=out_tform_goal.x, goal_y=out_tform_goal.y, goal_heading=out_tform_goal.angle,
-        frame_name=frame_name, params=RobotCommandBuilder.mobility_params(stair_hint=stairs))
-    end_time = 10.0
-    cmd_id = robot_command_client.robot_command(lease=None, command=robot_cmd,
-                                                end_time_secs=time.time() + end_time)
-    # Wait until the robot has reached the goal.
-    while True:
-        feedback = robot_command_client.robot_command_feedback(cmd_id)
-        mobility_feedback = feedback.feedback.synchronized_feedback.mobility_command_feedback
-        if mobility_feedback.status != RobotCommandFeedbackStatus.STATUS_PROCESSING:
-            print('Failed to reach the goal')
-            return False
-        traj_feedback = mobility_feedback.se2_trajectory_feedback
-        if (traj_feedback.status == traj_feedback.STATUS_AT_GOAL and
-                traj_feedback.body_movement_status == traj_feedback.BODY_STATUS_SETTLED):
-            print('Arrived at the goal.')
-            return True
-        time.sleep(1)
-
-    return True
-
-if __name__ == '__main__':
+if (__name__ == '__main__'):
     if not main():
         sys.exit(1)
